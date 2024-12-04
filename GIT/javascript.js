d4d8ea8067683e695c26df83a2e3c99b2b34fca9
Your Repo-Specific Username <your-repo-specific-email@example.com>
2024-12-04 18:27:59 +0530

    name

---
+++ GIT/javascript.js
@@ -0,0 +1,64 @@
+// strings = squance of characters 
+// let name = `my 
+// is 
+// dinesh
+// suthar`
+// console.log(name);
+
+// let firstname = new String("love babbar");
+// console.log(firstname);
+
+// let op1 = 'english';
+// let op2 = 'maths';
+// let ans = op1 +op2 ;
+// console.log(ans);
+
+// let op1 = 'english';
+// let op2 = 'maths';
+// let finalans = `${op1} + ${op2}`;
+// console.log(finalans);
+
+// let op1 = 'english';
+// let op2 = 'HINDI';
+// let finalans = `${op1}${op2}`;
+// console.log(finalans.toLowerCase());
+
+// let op1 = 'english';
+// let op2 = 'HINDI';
+// let finalans = `${op1}${op2}`;
+// console.log(finalans.substring(2,4));
+
+// let sntence = 'my name is dinesh i am for ahemdabad';
+// let words = sntence.split(' ');
+// console.log(words);
+
+// let sntence =" my \"name\" is dinesh i am for ahemdabad";
+// let words = sntence.split(' ');
+// console.log(words);
+
+// let sentence = "hello friends i hope you are very good";
+// let index = 5;
+// console.log(` index of ${index} return of character `);
+// console.log(sentence.charAt(5));
+
+// let one = "dineshe"
+// let two = "suthar"
+// console.log(one.concat(' ',two));
+
+
+
+
+
+// const str1 = 'Cats are the best!';
+
+// console.log(str1.endsWith('best!'));
+// Expected output: true
+
+// console.log(str1.endsWith('best', 17));
+// Expected output: true
+
+// const str2 = 'Is this a question?';
+
+// console.log(str2.endsWith('question'));
+
+
